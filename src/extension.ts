6080adbd46213ee5f824b1b87cf64ba6620a13a8
cooolbros <pfwobcke@gmail.com>
2024-06-30 23:34:32 +1000

    Use registerTextEditorCommand

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,13 @@ import { TextEditor, TextEditorEdit, commands, type ExtensionContext } from "vsc
 export function activate(context: ExtensionContext) {
 
 	context.subscriptions.push(
-		commands.registerCommand("decodeURIComponent", (editor: TextEditor, edit: TextEditorEdit) => {
+		commands.registerTextEditorCommand("decodeURIComponent", (editor: TextEditor, edit: TextEditorEdit) => {
 			edit.replace(editor.selection, decodeURIComponent(editor.document.getText(editor.selection)))
 		})
 	)
 
 	context.subscriptions.push(
-		commands.registerCommand("encodeURIComponent", (editor: TextEditor, edit: TextEditorEdit) => {
+		commands.registerTextEditorCommand("encodeURIComponent", (editor: TextEditor, edit: TextEditorEdit) => {
 			edit.replace(editor.selection, encodeURIComponent(editor.document.getText(editor.selection)))
 		})
 	)
