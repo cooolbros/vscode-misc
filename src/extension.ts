576b3ec7417f49d7b30418f28cd807190cf6dd58
cooolbros <pfwobcke@gmail.com>
2024-11-19 23:06:05 +1100

    Add randomUUID

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto"
 import { TextEditor, TextEditorEdit, commands, type ExtensionContext } from "vscode"
 
 export function activate(context: ExtensionContext) {
@@ -17,6 +18,14 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(
 		commands.registerTextEditorCommand("secure", (editor: TextEditor, edit: TextEditorEdit) => {
 			edit.replace(editor.selection, "X".repeat(editor.document.getText(editor.selection).length))
+		})
+	)
+
+	context.subscriptions.push(
+		commands.registerTextEditorCommand("randomUUID", (editor: TextEditor, edit: TextEditorEdit) => {
+			for (const selection of editor.selections) {
+				edit.insert(selection.start, randomUUID())
+			}
 		})
 	)
 }
